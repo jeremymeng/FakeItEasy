f7cb26c130e2f78ccea5c9ebba4666853d33c1a0
Jeremy Meng <yumeng@microsoft.com>
2016-02-08 14:09:27 -0800

    Add Type.GetTypeInfo() extension method for legacy platforms.

---
+++ Source/FakeItEasy.Net35/Compatibility/ReflectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FakeItEasy
+{
+    internal static class ReflectionExtensions
+    {
+        /// <summary>
+        /// A passthrough extension for legacy platforms that lacks GetTypeInfo() extension method.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static Type GetTypeInfo(this Type type)
+        {
+            return type;
+        }
+    }
+}
