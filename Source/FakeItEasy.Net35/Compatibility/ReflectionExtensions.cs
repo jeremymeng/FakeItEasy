aa3ac1134da3bb324d90cf2cbced1b7602c7b7d1
Jeremy Meng <yumeng@microsoft.com>
2016-02-09 10:56:53 -0800

    Add an extension method GetRuntimeInterfaceMap() for legacy platforms.

--- Source/FakeItEasy.Net35/Compatibility/ReflectionExtensions.cs
+++ Source/FakeItEasy.Net35/Compatibility/ReflectionExtensions.cs
@@ -15,9 +15,24 @@ namespace FakeItEasy
             return type;
         }
 
+        /// <summary>
+        /// This allows the usage of new Reflection Api Delegate.GetMethodInfo() on .NET 3.5 and 4.0.
+        /// It delegates to the old Delegate.Method property.
+        /// </summary>
+        /// <param name="delegate"></param>
+        /// <returns></returns>
         public static MethodInfo GetMethodInfo(this Delegate @delegate)
         {
             return @delegate.Method;
+        }
+
+        /// <summary>
+        /// This allows the usage of new Reflection Api Type.GetRuntimeInterfaceMap() on .NET 3.5 and 4.0.
+        /// It delegates to the old Type.GetInterfaceMap() method.
+        /// </summary>
+        public static InterfaceMapping GetRuntimeInterfaceMap(this Type type, Type interfaceType)
+        {
+            return type.GetInterfaceMap(interfaceType);
         }
     }
 }
